441e10bc067ef6510aa8111c1b7da9deeca285ff
jcgagne <jean-christian.gagne@ne.ch>
2025-11-28 19:21:14 +0100

    Gateway: config oidc pour docker

---
+++ src/main/java/ch/bdt/spike/spring/cloud/gatewayapplication/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package ch.bdt.spike.spring.cloud.gatewayapplication.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+@Slf4j
+public class SecurityConfig {
+    @Value("${spring.application.version}")
+    private String version;
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        log.info("SecurityConfig building security filter chain - v" + version);
+        http
+                // Désactive CSRF pour simplifier (à revoir en production)
+                .csrf(csrf -> csrf.disable())
+
+                // 1. Autoriser/Sécuriser les requêtes
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated())
+
+                // 2. Activer l'authentification OAuth2/OIDC (avec redirection vers Keycloak)
+                // Ceci active le filtre qui intercepte les requêtes non authentifiées
+                // et les redirige vers l'URI d'autorisation Keycloak.
+                .oauth2Login(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    //@Bean
+    //public String myDummyBean() {
+    //    throw new RuntimeException("Dummy bean");
+    //}
+
+
+}
